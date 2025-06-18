39ab902432d411e8cf5292c6fe6e36a3f3984502
¨Chege <cochewaki@gmail.com>
2025-06-18 14:09:35 +0300

    complete labs

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, -1, -1):
+        if i == 0:
+            print("Happy New Year!")
+        else:
+            print(i)
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for i in int_list:
+        squared_list.append(i**2)
+    return squared_list
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz()
